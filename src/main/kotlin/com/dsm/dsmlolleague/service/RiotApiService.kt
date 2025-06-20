e3ce80a3ae9fa8fa99f76f50a92d8cc5b0947bca
HyunSu1768 <azxcv1768@gmail.com>
2025-06-21 00:15:47 +0900

    Remove 'name' field from SummonerDto data class

--- src/main/kotlin/com/dsm/dsmlolleague/service/RiotApiService.kt
+++ src/main/kotlin/com/dsm/dsmlolleague/service/RiotApiService.kt
@@ -545,7 +545,6 @@ data class SummonerDto(
     val accountId: String,
     val profileIconId: Int,
     val revisionDate: Long,
-    val name: String,
     val id: String,
     val puuid: String,
     val summonerLevel: Int
